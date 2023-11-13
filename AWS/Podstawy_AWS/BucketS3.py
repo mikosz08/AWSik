4fc4fb5509817ae5a42f85f15b86c1b247664150
mikosz08 <ziolek.mikosz@protonmail.com>
2023-11-13 19:03:17 +0100

    adds snippet with buckets3

---
+++ AWS/Podstawy_AWS/BucketS3.py
@@ -0,0 +1,19 @@
+﻿import os
+import boto3
+import random
+import string
+
+
+class BucketS3:
+    def create_and_send_file(self):
+        file_path = "file_for_bucket.txt"
+
+        bucket_name = "my-bucket-mqs"
+
+        s3 = boto3.client("s3")
+        object_key = "file_for_bucket.txt"
+        s3.upload_file(file_path, bucket_name, object_key)
+
+        print(
+            f"Plik {file_path} został przesłany do pojemnika {bucket_name} jako {object_key}."
+        )
--- AWS/Podstawy_AWS/Podstawy_AWS.py
+++ AWS/Podstawy_AWS/Podstawy_AWS.py
@@ -1,6 +1,7 @@
 ﻿from LambdaAWS import LambdaAWS
 from SimpleQueueService import SimpleQueueService
 from DynamoDB import DynamoDB
+from BucketS3 import BucketS3
 
 
 def test_lambdas():
@@ -21,7 +22,13 @@ def test_dynamo():
     dynamo.put_item()
 
 
+def test_bucket():
+    bucket = BucketS3()
+    bucket.create_and_send_file()
+
+
 if __name__ == "__main__":
-    test_lambdas()
+    # test_lambdas()
     # test_sqs()
     # test_dynamo()
+    test_bucket()
